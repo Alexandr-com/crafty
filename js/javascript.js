a7a27d8c1c6695f0280bf434b3ab7a00bc2ddc85
Alexandr11113 <lex882613@gmail.com>
2021-09-30 02:37:02 +0300

    First commit

---
+++ js/javascript.js
@@ -0,0 +1,36 @@
+let menuBtn = document.querySelector('.menu__btn');
+let menuNav = document.querySelector('.menu-nav');
+let menuNavActive = document.querySelector('.menu-nav__activ');
+
+// Выезжающе меню из кнопки "меню-бургер"
+function activeMenu() {
+    menuNav.classList.toggle('menu-nav__activ');
+}
+menuBtn.addEventListener('click', activeMenu);
+
+
+// Меню-бургер
+function menuBurger() {
+    menuBtn.classList.toggle('change');
+}
+menuBtn.addEventListener('click', menuBurger);
+
+
+// Меню выезжающее с левой стороны экрана на экранах меньше 568px
+let menuBtn_left = document.querySelector('.btn-left');
+
+function activeMenu_left() {
+    document.querySelector('.menu-left').classList.toggle('menu-left__active');
+}
+menuBtn_left.addEventListener('click', activeMenu_left);
+
+
+// Код для contact-form.scss
+
+function closeMenu() {
+    menuNav.classList.remove('menu-nav__activ');
+    menuBtn.classList.remove('change');
+}
+document.querySelector('html').addEventListener('dblclick', closeMenu);
+document.querySelector('html').addEventListener('touchmove', closeMenu);
+window.onscroll = closeMenu;
